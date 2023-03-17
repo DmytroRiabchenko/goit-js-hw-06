0017514a09e7323289ab6e39b33adaf32e811b2d
DmytroRiabchenko <dmpmpk@gmail.com>
2023-03-17 19:07:14 +0100

    dz6

--- src/src/js/task-06.js
+++ src/src/js/task-06.js
@@ -3,3 +3,16 @@
 // Яка кількість символів повинна бути в інпуті, зазначається в його атрибуті data-length.
 // Якщо введена правильна кількість символів, то border інпуту стає зеленим, якщо неправильна кількість - червоним.
 // Для додавання стилів використовуй CSS-класи valid і invalid, які ми вже додали у вихідні файли завдання.
+
+const input = document.querySelector("#validation-input");
+console.log(input);
+
+input.addEventListener("blur", () => {
+  if (input.value.length === parseInt(input.dataset.length)) {
+    input.classList.remove("invalid");
+    input.classList.add("valid");
+  } else {
+    input.classList.remove("valid");
+    input.classList.add("invalid");
+  }
+});
